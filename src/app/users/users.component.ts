dd27e1bf8b7d227fd92ae8644c5bfddff78e4bca
Mohammed Sayed <farsfhd123@gmail.com>
2023-11-13 18:30:20 +0200

    second commit

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+/*
+to implement navigation programmaticlly:
+  1. need to inject Router in constructor
+  2. use Router methods in the method that will excute when the button clicked
+*/
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss']
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
+
+  constructor(private router: Router) {};
+
+  ngOnInit(): void {
+    
+  }
+
+  onCategoriesClick() {
+    // perform some logic
+    // navigate to page
+
+    /* use navigateByUrl when writing path staticlly */
+    this.router.navigateByUrl('/categories');
+
+    /* use navigate when writing path dynamiclly */
+    // this.router.navigate(['/categories', id])
+  }
 
 }
